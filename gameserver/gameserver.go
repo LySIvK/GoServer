6eec3eb0feab330c69ae510db28cef6eca08efb4
LySIvK <sky920929@qq.com>
2015-12-08 18:13:54 +0800

    design gameserver class

--- gameserver/gameserver.go
+++ gameserver/gameserver.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"gopath/code.google.com/p/go.net/websocket"
 )
 
 func Test() {
@@ -9,7 +10,25 @@ func Test() {
 }
 
 type GameServer struct {
-	serverID            int      //! 游戏服务器ID
-	addPlayerChannel    chan int //! 客户端登入通道
-	removePlayerChannel chan int //! 客户端登出通道
+	serverID            int          //! 游戏服务器ID
+	addPlayerChannel    chan *Player //! 客户端登入通道
+	removePlayerChannel chan *Player //! 客户端登出通道
+}
+
+//! 初始化服务器
+func (self *GameServer) Init(serverID int) {
+	self.serverID = serverID
+	self.addPlayerChannel = make(chan *Player)
+	self.removePlayerChannel = make(chan *Player)
+}
+
+//! 服务器连接回调
+func (self *GameServer) GetConnectHandler() websocket.Handler {
+
+}
+
+//! 生成一个新的游戏服务器
+func NewGameServer() *GameServer {
+	server := new(GameServer)
+	return server
 }
