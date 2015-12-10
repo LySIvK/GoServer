da287e88b8dc574745e761cc49aeee94287b1e18
LySIvK <sky920929@qq.com>
2015-12-10 17:09:10 +0800

    var some const value to save database name or table

---
+++ gameserver/table/dbtable.go
@@ -0,0 +1,9 @@
+package table
+
+const (
+	GameDB = "Game" //! 数据库名
+)
+
+const (
+	PlayerInfoTable = "PlayerInfo" //! 表名
+)
---
+++ loginserver/table/dbtable.go
@@ -0,0 +1,9 @@
+package table
+
+const (
+	AccountDB = "Account" //! 数据库名
+)
+
+const (
+	AccountInfoTable = "AccountsInfo" //! 账号信息表
+)
