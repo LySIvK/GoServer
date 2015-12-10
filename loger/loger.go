2bb0b9ec079921d5ba270f014408835d1ccc4ee4
LySIvK <sky920929@qq.com>
2015-12-10 14:19:49 +0800

    add a mutex lock for multi coroutine output

--- loger/loger.go
+++ loger/loger.go
@@ -5,6 +5,7 @@ import (
 	"gopath/github.com/issue9/term/colors"
 	"log"
 	"os"
+	"sync"
 	"time"
 )
 
@@ -24,29 +25,34 @@ var currentDay int      //! 当前天
 var logPath string      //! 当前日志文件路径
 var fileName string     //! 日志文件名字
 
+var outputLock sync.RWMutex
+
 func Output(logType int, format string, v ...interface{}) { ///信息
 	if logType < LogMinLevel {
 		return ///过滤此等级
 	}
+	outputLock.Lock()
 	//colorDic := map[string]string{"[Debug]": CLR_G, "[Info]": CLR_B, "[Warn]": CLR_Y, "[Error]": CLR_R, "[Fatal]": CLR_P}
 	prefixStr := "unknow"
 	c := colors.New(colors.Stdout, colors.White, colors.Black)
+	c.SetColor(colors.White, colors.Black)
 	switch logType { //! 根据消息类型自由搭配颜色
 	case LogDebug:
 		prefixStr = "[Debug]"
+		c.SetColor(colors.Green, colors.Black)
 		c = colors.New(colors.Stdout, colors.Green, colors.Black)
 	case LogInfo:
 		prefixStr = "[Info]"
-		c = colors.New(colors.Stdout, colors.White, colors.Black)
+		c.SetColor(colors.Cyan, colors.Black)
 	case LogWarn:
 		prefixStr = "[Warn]"
-		c = colors.New(colors.Stdout, colors.Yellow, colors.Black)
+		c.SetColor(colors.Yellow, colors.Black)
 	case LogError:
 		prefixStr = "[Error]"
-		c = colors.New(colors.Stdout, colors.Red, colors.Black)
+		c.SetColor(colors.Red, colors.Black)
 	case LogFatal:
 		prefixStr = "[Fatal]"
-		c = colors.New(colors.Stdout, colors.Red, colors.White)
+		c.SetColor(colors.Red, colors.White)
 	}
 	s := fmt.Sprintf(prefixStr+format, v...)
 	out := fmt.Sprintf(format, v...)
@@ -56,6 +62,8 @@ func Output(logType int, format string, v ...interface{}) { ///信息
 		c.Printf(prefixStr)
 		std.Output(3, out)
 	}
+
+	outputLock.Unlock()
 }
 
 func Print(format string, v ...interface{}) { //! Print
